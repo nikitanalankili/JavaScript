da01135fd8d20674f75dacc76137cf891a2a498e
nikitanalankili <nikitanalan2003@gmail.com>
2024-01-22 12:07:45 +0530

    JAVA SCRIPT

--- set.js
+++ set.js
@@ -11,3 +11,18 @@ setExample = new Set(["aiadmk","bjk","congress"])
 setExample.add("dmk")
 setExample.add("mkl")
 console.log(setExample)
+for(element of setExample)
+{
+    console.log("The element is ",element)
+}
+console.log("\n")
+
+//MAP
+mapExample = new Map([["ID","21ECR132"],["Name","Nikita"]])
+console.log(mapExample)
+for(element of mapExample)
+{
+    console.log("The element is ",element)
+}
+console.log("\n")
+console.log(mapExample.has("Name"))
