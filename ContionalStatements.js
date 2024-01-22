d8b0d960f456f75ced540f8d219ba901e44367ae
nikitanalankili <nikitanalan2003@gmail.com>
2024-01-22 14:22:46 +0530

    JAVA SCRIPT

--- ContionalStatements.js
+++ ContionalStatements.js
@@ -8,3 +8,23 @@ else
 {
     console.log("Please wait for appropriate age .")
 }
+ //Nested if else positive even or odd or negative or zero
+  var a = 10
+  if(a==0)
+  {
+    console.log("The number is Zero")
+  }
+  if(a>0)
+  {
+    if(a%2==0)
+    {
+        console.log("The number is a positive even number")
+    }
+    else{
+        console.log("The number is a positive odd number ")
+    }
+ }
+ if(a<0)
+ {
+    console.log("The number is a negative number")
+ }
